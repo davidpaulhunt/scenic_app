e125fe82a9025402ceb280e64c1a61b299298aab
txdavidtx <davidpaulhunt@gmail.com>
2014-05-12 19:55:52 -0400

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   def current_user
     unless params['_method'] == "delete"
       if cookies[:remember_token].present?
-        @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
       end
     end
   end
