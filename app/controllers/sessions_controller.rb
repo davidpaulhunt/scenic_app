5bdf051552e63aedd234831ac63aff1d9313f9d2
txdavidtx <davidpaulhunt@gmail.com>
2014-05-12 20:06:08 -0400

    found path_boundry as a block

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   skip_filter :ensure_logged_in, only: [:new, :create, :destroy]
-  before_filter :path_boundary, only: [:new, :create, :destroy]
+  before_filter :path_boundary, only: [:new, :create]
   
   def new
   end
